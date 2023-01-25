fd07e3dc6f4d071413d3a1570abb500f3b900fbb
Kavyar2804 <kavyar2804@gmail.com>
2023-01-25 17:47:31 +0530

    First commit

---
+++ datatype.js
@@ -0,0 +1,15 @@
+var a=10
+console.log(typeof(a)) //number
+console.log(typeof(String(a))) //to change datatype explicitily
+
+var b ="KAvya"
+console.log(typeof(b)) //string
+
+var c =true
+console.log(typeof(c)) //boolean 
+
+var d                   //variable not intialized
+console.log(typeof(d)) //undefined
+
+console.log(typeof(e)) //undefined
+
